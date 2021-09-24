ce91d73b02d5ec9f62cdb24e09a7a9a7b1c0713a
Tonomatic <antonysoly2217@gmail.com>
2021-09-23 21:32:44 -0400

    working on the mongoose data model

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+//routes
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 mongoose
   .connect(db, { useNewUrlParser: true })
